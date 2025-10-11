83e4c784f6282fab32a7fb9193a7a200beacd976
Eric Atsu <ericatsu29@gmail.com>
2025-10-11 17:55:29 +0000

    Add GitHub Actions for Windows build

--- lib/app/core/utils/constants.dart
+++ lib/app/core/utils/constants.dart
@@ -5,11 +5,6 @@ import 'package:yesudua_ventures/app/core/utils/assets.dart';
 class AppConstants {
   static const String appName = "Yesu Dea Ventures IMS";
 
-  // Supabase
-  static const String supabaseUrl = "https://zrojtggzeaczgqgbolna.supabase.co";
-  static const String supabaseAnonKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpyb2p0Z2d6ZWFjemdxZ2JvbG5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MTExOTksImV4cCI6MjA2MDQ4NzE5OX0.a6XVZnaxLUzjWQw-AZhuA9cHkg2s4G8QCPa9lC355kw";
-
   // Drift DB
   static const String localDbName = "inventory_management.db";
 
